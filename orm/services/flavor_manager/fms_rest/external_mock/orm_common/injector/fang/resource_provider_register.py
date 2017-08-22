3543a6589396e8b010871724bbb390dc05a68805
raigax9 <nishad.shah@att.com>
2017-08-22 15:39:40 -0400

    Added remaining fully qualified import names in injector (flavor_manager)
    Updated import names to be fully qualified as well as removing
    logger "TODO" from injector since it was already added before.
    
    Change-Id: I0d0e59db415af6b81e8a274c479e8519f5f311aa

--- orm/services/flavor_manager/fms_rest/external_mock/orm_common/injector/fang/resource_provider_register.py
+++ orm/services/flavor_manager/fms_rest/external_mock/orm_common/injector/fang/resource_provider_register.py
@@ -1,7 +1,6 @@
 from functools import partial
 
-from .errors import (FangError, ProviderAlreadyRegisteredError,
-                     ProviderNotFoundError)
+from orm.services.flavor_manager.fms_rest.external_mock.orm_common.injector.fang.errors import (FangError, ProviderAlreadyRegisteredError, ProviderNotFoundError)
 
 
 class ResourceProviderRegister:
