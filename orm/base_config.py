3f445359bdcfda9c5625415a590cbb75f7575f8d
hosingh000 <hosingh000@gmail.com>
2017-09-21 11:33:42 -0500

    setup and install ranger
    Change-Id: I20a1091eb052d7540440e32cc9c802272fd4ae5c

---
+++ orm/base_config.py
@@ -0,0 +1,9 @@
+import os
+orm_host = '0.0.0.0'
+log_location = '{}'
+ranger_base = os.path.dirname(os.path.abspath('orm'))
+log_location = ranger_base+'/logs/{}'
+uuid = {
+    'port': '7001',
+    'log': log_location.format('uuidgen.log')
+}
