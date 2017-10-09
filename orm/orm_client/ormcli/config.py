892a39ce98f470377ad0186069940dfdd7e4cd65
raigax9 <nishad.shah@att.com>
2017-10-09 10:13:37 -0400

    Added missing 'v1' in rds config file and updated ormcli config
    The UUID_URL was missing a 'v1' that was causing an
    issue for running rds. Updated ormcli config file
    where the base_url is changed.
    
    Change-Id: Ifed2d94332eddd4ca200008ad397bba4c512adb0

--- orm/orm_client/ormcli/config.py
+++ orm/orm_client/ormcli/config.py
@@ -6,5 +6,5 @@ tenant_name = config.token_auth_tenant
 username = config.token_auth_user
 password = config.token_auth_pass
 auth_region = config.cli['base_region']
-orm_base_url = config.rms['base_url']
+orm_base_url = 'http://127.0.0.1'
 verify = config.ssl_verify
