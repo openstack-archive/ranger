5f64e0548c44a4065e2cf68e4eac842b66208a64
stewie925 <st3wty@att.com>
2018-05-21 20:19:08 -0500

    Add Ranger Tempest Tests
    Change-Id: Ie96bad932c6041c0b2781e4fecdc1098cdb5f2fc

---
+++ ranger_tempest_tests/aic-orm-tempest-plugin/aic_orm_tempest_plugin/services/fms_client.py
@@ -0,0 +1,169 @@
+# Copyright 2016 AT&T Corp
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+
+from aic_orm_tempest_plugin.schemas import flavors_schema as schema
+from aic_orm_tempest_plugin.services import base_client
+
+from tempest import config
+from tempest.lib.common import rest_client
+
+CONF = config.CONF
+
+
+class FmsClient(base_client.OrmClientBase):
+
+    fms_url = '%s:%s' % (CONF.orm.uri, CONF.orm.fms_port)
+    version = "v1"
+
+    def get_extra_headers(self):
+        headers = {'X-Auth-Region': CONF.identity.region,
+                   'X-AIC-ORM-Tracking-Id': 'test',
+                   'X-AIC-ORM-Requester': CONF.auth.admin_username,
+                   'X-AIC-ORM-Client': 'cli'
+                   }
+        return headers
+
+    def create_flavor(self, **kwargs):
+        uri = '%s/%s/orm/flavors' % (self.fms_url, self.version)
+        post_body = {"flavor": kwargs}
+        post_body = json.dumps(post_body)
+        # ex_headers = self.get_headers()
+        # resp, body = self.post(uri, body=post_body,
+        #                       extra_headers=ex_headers)
+        # body = json.loads(body)
+        # self.validate_response(schema.create_flavor, resp, body)
+        # return rest_client.ResponseBody(resp, body["flavor"])
+        return self.post_request(uri, post_body, schema.create_flavor)
+
+    def get_flavor(self, identifier, para=None):
+        if para is None:
+            uri = '%s/%s/orm/flavors/%s' % (self.fms_url, self.version,
+                                            identifier)
+        else:
+            uri = '%s/%s/orm/flavors/%s/%s' % (self.fms_url, self.version,
+                                               identifier, para)
+        return self.get_request(uri, schema.get_flavor)
+
+    def list_flavors(self, para=None):
+        if para is None:
+            uri = '%s/%s/orm/flavors' % (self.fms_url, self.version)
+        else:
+            uri = '%s/%s/orm/flavors/%s' % (self.fms_url, self.version, para)
+        # ex_headers = self.get_headers()
+        # resp, body = self.get(url, extra_headers=ex_headers)
+        # self.expected_success(200, resp.status)
+        # body = json.loads(body)
+        # self.validate_response(schema.list_flavors, resp, body)
+        # return rest_client.ResponseBody(resp, body)
+        return self.get_request(uri, schema.list_flavors)
+
+    def delete_region_from_flavor(self, flavor_id, region_id):
+        uri = '%s/%s/orm/flavors/%s/regions/%s' % (self.fms_url,
+                                                   self.version, flavor_id,
+                                                   region_id)
+        ex_headers = self.get_headers()
+        resp, body = self.delete(uri, extra_headers=ex_headers)
+        self.expected_success(204, resp.status)
+        return rest_client.ResponseBody(resp, body)
+
+    def delete_flavor(self, flavor_id):
+        uri = '%s/%s/orm/flavors/%s' %\
+            (self.fms_url, self.version, flavor_id)
+        return self.delete_request(uri, schema.delete_flavor)
+
+    def delete_tags(self, flavor_id, para):
+        if para is None:
+            uri = '%s/%s/orm/flavors/%s/tags' % (self.fms_url, self.version,
+                                                 flavor_id)
+        else:
+            uri = '%s/%s/orm/flavors/%s/tags/%s' % (self.fms_url, self.version,
+                                                    flavor_id, para)
+        return self.delete_request(uri, schema.delete_tags)
+
+    def get_tags(self, flavor_id):
+        uri = '%s/%s/orm/flavors/%s/tags' % (self.fms_url, self.version,
+                                             flavor_id)
+        return self.get_request(uri, schema.get_tags)
+
+    def add_tags(self, flavor_id, tag_body):
+        uri = '%s/%s/orm/flavors/%s/tags' % (self.fms_url, self.version,
+                                             flavor_id)
+        post_body = json.dumps(tag_body)
+        return self.post_request(uri, post_body, schema.add_tags)
+
+    def update_tags(self, flavor_id, tag_body):
+        uri = '%s/%s/orm/flavors/%s/tags' % (self.fms_url, self.version,
+                                             flavor_id)
+        put_body = json.dumps(tag_body)
+        return self.put_request(uri, put_body, schema.update_tags)
+
+    def get_extra_specs(self, flavor_id):
+        uri = '%s/%s/orm/flavors/%s/os_extra_specs' % (self.fms_url,
+                                                       self.version,
+                                                       flavor_id)
+        return self.get_request(uri, schema.get_extra_specs)
+
+    def add_flvr_tenants(self, flavor_id, tenant_body):
+        uri = '%s/%s/orm/flavors/%s/tenants/' % (self.fms_url,
+                                                 self.version,
+                                                 flavor_id)
+        post_body = json.dumps(tenant_body)
+        return self.post_request(uri, post_body, schema.add_tenant)
+
+    def add_flvr_regions(self, flavor_id, region_body):
+        uri = '%s/%s/orm/flavors/%s/regions' % (self.fms_url,
+                                                self.version,
+                                                flavor_id)
+        post_body = json.dumps(region_body)
+        return self.post_request(uri, post_body, schema.add_region)
+
+    def delete_flvr_region(self, flavor_id, region_id):
+        uri = '%s/%s/orm/flavors/%s/regions/%s' % (self.fms_url,
+                                                   self.version,
+                                                   flavor_id, region_id)
+        return self.delete_request(uri, schema.delete_region)
+
+    def add_extra_specs(self, flavor_id, extra_specs_body):
+        uri = '%s/%s/orm/flavors/%s/os_extra_specs' % (self.fms_url,
+                                                       self.version,
+                                                       flavor_id)
+        post_body = json.dumps(extra_specs_body)
+        return self.post_request(uri, post_body, schema.add_extra_specs)
+
+    def update_extra_specs(self, flavor_id, extra_specs_body):
+        uri = '%s/%s/orm/flavors/%s/os_extra_specs' % (self.fms_url,
+                                                       self.version,
+                                                       flavor_id)
+        put_body = json.dumps(extra_specs_body)
+        return self.put_request(uri, put_body, schema.update_extra_specs)
+
+    def delete_extra_specs(self, flavor_id, para):
+        if para is None:
+            uri = '%s/%s/orm/flavors/%s/os_extra_specs' % (self.fms_url,
+                                                           self.version,
+                                                           flavor_id)
+        else:
+            uri = '%s/%s/orm/flavors/%s/os_extra_specs/%s' % (self.fms_url,
+                                                              self.version,
+                                                              flavor_id, para)
+        return self.delete_request(uri, schema.delete_extra_specs)
+
+    def delete_flvr_tenant(self, flavor_id, tenant):
+        uri = '%s/%s/orm/flavors/%s/tenants/%s' % (self.fms_url,
+                                                   self.version,
+                                                   flavor_id, tenant)
+        return self.delete_request(uri, schema.delete_tenant)
---
+++ ranger_tempest_tests/aic-orm-tempest-plugin/aic_orm_tempest_plugin/services/rms_client.py
@@ -0,0 +1,159 @@
+# Copyright 2016 AT&T Corp
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+import urllib
+
+from aic_orm_tempest_plugin.schemas import regions_schema as schema
+from aic_orm_tempest_plugin.services import base_client
+
+from tempest import config
+
+
+CONF = config.CONF
+
+
+class RmsClient(base_client.OrmClientBase):
+
+    rms_url = '%s:%s' % (CONF.orm.uri, CONF.orm.region_port)
+    version = "v2"
+
+    def create_region(self, region_id, **kwargs):
+        uri = '%s/%s/orm/regions' % (self.rms_url, self.version)
+        post_body = {
+            'status': 'functional',
+            'name': region_id,
+            'id': region_id,
+            'description': region_id,
+            'designType': 'compact',
+            'locationType': 'testlocation',
+            'vlcpName': 'testvlcp',
+            'address': {
+                'country': 'usa',
+                'state': 'tx',
+                'city': 'austin',
+                'street': '12 main',
+                'zip': '12345'
+            },
+            'metadata': {
+                'key': ["value"]
+            },
+            'endpoints': [
+                {
+                    'publicURL':
+                        'https://dashboard-aic.%s.cci.att.com' % region_id,
+                    'type': 'dashboard'
+                },
+                {
+                    'publicURL':
+                        'https://identity-aic.%s.cci.att.com:5000' % region_id,
+                    'type': 'identity'
+                },
+                {
+                    'publicURL':
+                        'https://ord-aic.%s.cci.att.com:9010' % region_id,
+                    'type': 'ord'
+                },
+
+            ],
+            'aicVersion': '3.6',
+            'OSVersion': 'kilo',
+            'CLLI': 'testclli'
+        }
+        if kwargs is not None:
+            for key in kwargs:
+                post_body[key] = kwargs[key]
+
+        post_body = json.dumps(post_body)
+        return self.post_request(uri, post_body, schema.create_region)
+
+    def update_region(self, region_id, **kwargs):
+        uri = '%s/%s/orm/regions/%s' % (self.rms_url, self.version, region_id)
+        put_body = json.dumps(kwargs)
+        return self.put_request(uri, put_body, schema.update_region)
+
+    def update_region_status(self, region_id, status):
+        uri = '%s/%s/orm/regions/%s/status' \
+            % (self.rms_url, self.version, region_id)
+        put_body = json.dumps(status)
+        return self.put_request(uri, put_body, schema.update_status)
+
+    def update_region_metadata(self, region_id, metadata):
+        uri = '%s/%s/orm/regions/%s/metadata' \
+            % (self.rms_url, self.version, region_id)
+        put_body = json.dumps(metadata)
+        return self.put_request(uri, put_body, schema.update_metadata)
+
+    def get_region(self, identifier):
+        uri = '%s/%s/orm/regions/%s' % (self.rms_url, self.version, identifier)
+        return self.get_request(uri, schema.get_region)
+
+    def get_region_metadata(self, identifier):
+        uri = '%s/%s/orm/regions/%s/metadata'\
+            % (self.rms_url, self.version, identifier)
+        return self.get_request(uri, schema.get_region_metadata)
+
+    def list_regions_v1(self):
+        uri = self.rms_url + '/lcp'
+        return self.get_request(uri, schema.list_region_v1)
+
+    def list_regions(self, filter=None):
+        uri = '%s/%s/orm/regions' % (self.rms_url, self.version)
+        if filter is not None:
+            uri += '?' + urllib.urlencode(filter)
+        return self.get_request(uri, schema.list_region)
+
+    def delete_region(self, region_id):
+        uri = '%s/%s/orm/regions/%s' % (self.rms_url, self.version, region_id)
+        return self.delete_request(uri, schema.delete_region)
+
+    def add_region_metadata(self, region_id, **kwargs):
+        uri = '%s/%s/orm/regions/%s/metadata'\
+            % (self.rms_url, self.version, region_id)
+        post_body = json.dumps(kwargs)
+        return self.post_request(uri, post_body, schema.update_metadata)
+#
+#    def delete_region_metadata(self, region_id, key):
+#        uri = '%s/%s/orm/regions/%s/metadata/%s' % (
+#            self.rms_url, self.version, region_id, key)
+#        ex_headers = self.get_headers()
+#        resp, body = self.delete(uri, extra_headers=ex_headers)
+#        self.expected_success(200, resp.status)
+#        body = json.loads(body)
+#        return rest_client.ResponseBody(resp, body)
+
+    def create_region_group(self, **kwargs):
+        uri = '%s/%s/orm/groups' % (self.rms_url, self.version)
+        post_body = json.dumps(kwargs)
+        return self.post_request(uri, post_body, schema.create_region_group)
+
+    def update_region_group(self, group_id, **kwargs):
+        uri = '%s/%s/orm/groups/%s' % (self.rms_url, self.version, group_id)
+        put_body = json.dumps(kwargs)
+        return self.put_request(uri, put_body, schema.update_region_group)
+
+    def get_region_group(self, identifier):
+        uri = '%s/%s/orm/groups/%s'\
+            % (self.rms_url, self.version, identifier)
+        return self.get_request(uri, schema.get_region_group)
+
+    def list_region_groups(self):
+        uri = '%s/%s/orm/groups' % (self.rms_url, self.version)
+        return self.get_request(uri, schema.list_region_groups)
+
+    def delete_region_group(self, region_group_id):
+        uri = '%s/%s/orm/groups/%s' % (self.rms_url, self.version,
+                                       region_group_id)
+        return self.delete_request(uri, schema.delete_region_group)
