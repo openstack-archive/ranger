5b8569fc075934ef4c8b05aa70db1a3b28b17e60
stewie925 <st3wty@att.com>
2018-08-17 13:08:51 -0500

    Fix Ranger issues
    Fix up Ranger Dockerfile and README.rst.
    Fix Dockerfile for tempest tests.
    Also remove unnecessary directories.
    Change-Id: I6d64755c6911919f8527e94a57113d886daf22bc

--- tools/start_services.sh
+++ tools/start_services.sh
@@ -4,31 +4,31 @@ set -e
 
 echo "Starting the audit service"
 
-nohup orm-audit > /dev/null 2>&1 &
+nohup orm-audit > /dev/null 2>&1&
 
 echo "Starting the uuidgen service"
 
-nohup orm-uuidgen > /dev/null 2>&1 &
+nohup orm-uuidgen > /dev/null 2>&1&
 
 echo "Starting the rds service"
 
-nohup orm-rds > /dev/null 2>&1 &
+nohup orm-rds > /dev/null 2>&1&
 
 echo "Starting the rms service"
 
-nohup orm-rms > /dev/null 2>&1 &
+nohup orm-rms > /dev/null 2>&1&
 
 echo "Starting the cms service"
 
-nohup orm-cms > /dev/null 2>&1 &
+nohup orm-cms > /dev/null 2>&1&
 
 echo "Starting the ims service"
 
-nohup orm-ims > /dev/null 2>&1 &
+nohup orm-ims > /dev/null 2>&1&
 
 
 echo "Starting the fms service"
 
-nohup orm-fms > /dev/null 2>&1 &
+nohup orm-fms > /dev/null 2>&1&
 
 
