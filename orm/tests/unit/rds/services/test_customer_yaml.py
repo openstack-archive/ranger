bcec2197be0a2d820e5dc40364df9da9b0c1912d
Mclean, Aaron (am9518) <am9518@att.com>
2018-08-30 08:24:26 -0500

    [US449915] NC 1.0 - Fixing CMS Heat Template for Ocata
    Change-Id: I5ef61ae240d1d86fd6130db1014348dc9fe08d2b

--- orm/tests/unit/rds/services/test_customer_yaml.py
+++ orm/tests/unit/rds/services/test_customer_yaml.py
@@ -93,7 +93,8 @@ fullyaml_with_users_quotasoff = \
     '- project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n        role: storage\n' \
     '    type: OS::Keystone::User\n\n      \n  1e24981a-fa51-11e5-86aa-5e5517507c66:\n    properties:\n      ' \
     'description: this is a description\n      enabled: true\n      ' \
-    'name: welcome_man\n      project_id: 1e24981a-fa51-11e5-86aa-5e5517507c66\n    type: OS::Keystone::Project2\n\n      ' \
+    'tags: [my_server_name=Apache1,ocx_cust=123456889]\n      ' \
+    'name: welcome_man\n      project_id: 1e24981a-fa51-11e5-86aa-5e5517507c66\n    type: OS::Keystone::Project\n\n      ' \
     '\n  1e24981a-fa51-11e5-86aa-5e5517507c66_userId1_group:\n    properties:\n      description: dummy\n      ' \
     'domain: default\n      name: 1e24981a-fa51-11e5-86aa-5e5517507c66_userId1_group\n      roles:\n      - ' \
     'project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n        role: {get_resource: other}\n    ' \
@@ -121,8 +122,9 @@ fullyaml_no_users_quotasoff = \
     'name: userId2zzz\n      roles:\n      - project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n' \
     '        role: storagezzzzz\n    type: OS::Keystone::User\n\n      ' \
     '\n  1e24981a-fa51-11e5-86aa-5e5517507c66:\n    properties:\n      description: this is a description\n' \
+    '      tags: [my_server_name=Apache1,ocx_cust=123456889]\n' \
     '      enabled: true\n      name: welcome_man\n    ' \
-    '  project_id: 1e24981a-fa51-11e5-86aa-5e5517507c66\n    type: OS::Keystone::Project2\n\n      \n  1e24981a-fa51-11e5-86aa-5e5517507c66_userId1zzzz_group:\n    ' \
+    '  project_id: 1e24981a-fa51-11e5-86aa-5e5517507c66\n    type: OS::Keystone::Project\n\n      \n  1e24981a-fa51-11e5-86aa-5e5517507c66_userId1zzzz_group:\n    ' \
     'properties:\n      description: dummy\n      domain: default\n      ' \
     'name: 1e24981a-fa51-11e5-86aa-5e5517507c66_userId1zzzz_group\n      roles:\n      - project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n' \
     '        role: {get_resource: otherzzzzz}\n    type: OS::Keystone::Group\n\n' \
@@ -136,14 +138,14 @@ fullyaml_no_users_quotasoff = \
 
 full_yaml_default_quotas = 'heat_template_version: 2015-1-1\n\ndescription: yaml file for region ' \
                            '- regionname\n\nresources:\n  cinder_quota:\n    properties:\n      ' \
-                           'gigabytes: 111\n      snapshots: 111\n      tenant: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n' \
+                           'gigabytes: 111\n      snapshots: 111\n      project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n' \
                            '      volumes: 111\n    type: OS::Cinder::Quota\n\n   ' \
                            '   \n  neutron_quota:\n    properties:\n      floatingip: 111\n' \
                            '      network: 111\n      port: 111\n      router: 111\n      subnet: 111\n' \
-                           '      tenant: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n    type: OS::Neutron::Quota\n\n' \
+                           '      project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n    type: OS::Neutron::Quota\n\n' \
                            '      \n  nova_quota:\n    properties:\n      injected_files: 111\n      ' \
                            'instances: 111\n      keypairs: 111\n      ram: 111\n      ' \
-                           'tenant: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n    ' \
+                           'project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n    ' \
                            'type: OS::Nova::Quota\n\n      \n  tenant_metadata:\n    properties:\n      METADATA:\n      ' \
                            '  metadata:\n          my_server_name: Apache1\n          ocx_cust: 123456889\n      ' \
                            'TENANT_ID: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n    ' \
@@ -159,9 +161,10 @@ full_yaml_default_quotas = 'heat_template_version: 2015-1-1\n\ndescription: yaml
                            'name: userId2\n      roles:\n      - project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n' \
                            '        role: storage\n    type: OS::Keystone::User\n\n      ' \
                            '\n  1e24981a-fa51-11e5-86aa-5e5517507c66:\n    properties:\n      description: this is a description\n' \
+                           '      tags: [my_server_name=Apache1,ocx_cust=123456889]\n' \
                            '      enabled: true\n      name: welcome_man\n' \
                            '      project_id: 1e24981a-fa51-11e5-86aa-5e5517507c66\n    ' \
-                           'type: OS::Keystone::Project2\n\n      \n  1e24981a-fa51-11e5-86aa-5e5517507c66_userId1_group:\n' \
+                           'type: OS::Keystone::Project\n\n      \n  1e24981a-fa51-11e5-86aa-5e5517507c66_userId1_group:\n' \
                            '    properties:\n      description: dummy\n      domain: default\n' \
                            '      name: 1e24981a-fa51-11e5-86aa-5e5517507c66_userId1_group\n      roles:\n' \
                            '      - project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n        ' \
@@ -178,14 +181,14 @@ full_yaml_default_quotas = 'heat_template_version: 2015-1-1\n\ndescription: yaml
 full_yaml_quotas = 'heat_template_version: 2015-1-1\n\ndescription: yaml file for region - ' \
                    'regionnametest\n\nresources:\n  cinder_quota:\n    ' \
                    'properties:\n      gigabytes: 10\n      snapshots: 10\n      ' \
-                   'tenant: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n      volumes: 10\n    ' \
+                   'project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n      volumes: 10\n    ' \
                    'type: OS::Cinder::Quota\n\n      \n  neutron_quota:\n    ' \
                    'properties:\n      floatingip: 10\n      network: 10\n      ' \
                    'port: 10\n      router: 10\n      subnet: 10\n      ' \
-                   'tenant: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n    ' \
+                   'project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n    ' \
                    'type: OS::Neutron::Quota\n\n      \n  nova_quota:\n    ' \
                    'properties:\n      injected_files: 10\n      instances: 10\n      ' \
-                   'keypairs: 10\n      ram: 10\n      tenant: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n    ' \
+                   'keypairs: 10\n      ram: 10\n      project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n    ' \
                    'type: OS::Nova::Quota\n\n      \n  tenant_metadata:\n    ' \
                    'properties:\n      METADATA:\n        metadata:\n          my_server_name: Apache1\n' \
                    '          ocx_cust: 123456889\n      TENANT_ID: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n' \
@@ -201,9 +204,10 @@ full_yaml_quotas = 'heat_template_version: 2015-1-1\n\ndescription: yaml file fo
                    'role: storagezzzzz\n    type: OS::Keystone::User\n\n' \
                    '      \n  1e24981a-fa51-11e5-86aa-5e5517507c66:\n    properties:\n      ' \
                    'description: this is a description\n      ' \
+                   'tags: [my_server_name=Apache1,ocx_cust=123456889]\n      ' \
                    'enabled: true\n      name: welcome_man\n      ' \
                    'project_id: 1e24981a-fa51-11e5-86aa-5e5517507c66\n    ' \
-                   'type: OS::Keystone::Project2\n\n      \n  1e24981a-fa51-11e5-86aa-5e5517507c66_userId1zzzz_group:\n' \
+                   'type: OS::Keystone::Project\n\n      \n  1e24981a-fa51-11e5-86aa-5e5517507c66_userId1zzzz_group:\n' \
                    '    properties:\n      description: dummy\n      ' \
                    'domain: default\n      name: 1e24981a-fa51-11e5-86aa-5e5517507c66_userId1zzzz_group\n      roles:\n      ' \
                    '- project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n        ' \
@@ -234,9 +238,10 @@ full_yaml_ldap = 'heat_template_version: 2015-1-2\n\ndescription: yaml file' \
                  'type: OS::Keystone::UserRoleAssignment\n\n      \n  ' \
                  '1e24981a-fa51-11e5-86aa-5e5517507c66:\n    properties:\n      ' \
                  'description: this is a description\n      ' \
+                 'tags: [my_server_name=Apache1,ocx_cust=123456889]\n      ' \
                  'enabled: true\n      name: welcome_man\n      ' \
                  'project_id: 1e24981a-fa51-11e5-86aa-5e5517507c66\n    ' \
-                 'type: OS::Keystone::Project2\n\n      \n\noutputs:\n  ' \
+                 'type: OS::Keystone::Project\n\n      \n\noutputs:\n  ' \
                  'userId1_id:\n    ' \
                  'value: {get_resource: userId1}\n  userId2_id:\n    ' \
                  'value: {get_resource: userId2}\n  1e24981a-fa51-11e5-86aa-5e5517507c66_id:\n    ' \
@@ -259,13 +264,13 @@ fullyaml_aic4 = \
     '      - project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n'\
     '        role: {get_resource: storage}\n    type: OS::Keystone::Group\n\n'\
     '      \n  cinder_quota:\n    properties:\n      gigabytes: 111\n      snapshots: 111\n'\
-    '      tenant: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n      volumes: 111\n'\
+    '      project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n      volumes: 111\n'\
     '    type: OS::Cinder::Quota\n\n      \n  neutron_quota:\n    properties:\n'\
     '      floatingip: 111\n      network: 111\n      port: 111\n      router: 111\n'\
-    '      subnet: 111\n      tenant: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n'\
+    '      subnet: 111\n      project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n'\
     '    type: OS::Neutron::Quota\n\n      \n  nova_quota:\n    properties:\n'\
     '      injected_files: 111\n      instances: 111\n      keypairs: 111\n'\
-    '      ram: 111\n      tenant: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n'\
+    '      ram: 111\n      project: {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66}\n'\
     '    type: OS::Nova::Quota\n\n      \n  userId1:\n    properties:\n      groups:\n'\
     '      - {get_resource: 1e24981a-fa51-11e5-86aa-5e5517507c66_userId1_group}\n'\
     '      name: userId1\n      roles:\n'\
