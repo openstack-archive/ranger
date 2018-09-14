b4f0497ec30055b157138dc03152429579babfa0
stewie925 <st3wty@att.com>
2018-09-14 10:56:44 -0700

    Ranger Changes -
    1. add database drop script
    2. fix uuidgen config
    3. fix logic to reset rangeragent version to 1.0
    4. rename ranger services in start_services.sh script
    
    Change-Id: Ib9bddaa0385295c3bdea0b3eed942d6b0f71cb59

--- orm/services/resource_distributor/config.py
+++ orm/services/resource_distributor/config.py
@@ -67,9 +67,9 @@ ordupdate = {
     'discovery_port': config.rms['port'],
     'template_type': 'hot',
     # This flag should be false only in case the ord does not support https.
-    'https_enabled': False,
+    'https_enabled': config.https_enabled,
     # ORD supports HTTPS and you don't need a certificate? set 'cert_path': ''
-    'cert_path': ''
+    'cert_path': config.cert_path
 }
 
 UUID_URL = config.uuid['base_url'] + 'v1/uuids'
@@ -99,17 +99,12 @@ yaml_configs = {
                 'subnets': 'subnet'
             }
         },
-        'cms_template_version': {
-            'resource_v1': {
-                'ranger_agent_version': 3.0,
-                'quota_unsupported_params': ['vips', 'members', 'pools',
-                                             'nat_instance', 'route_table',
-                                             'health_monitors', 'loadbalancer',
-                                             'listener']
-            },
-            'resource_v2': {
-                'ranger_agent_version': 3.6,
-                'quota_unsupported_params': []
+        'cms_quota': {
+            'resource_quotas': {
+                'quota_supported_params': ['vips', 'members', 'pools',
+                                           'nat_instance', 'route_table',
+                                           'health_monitors', 'loadbalancer',
+                                           'listener']
             }
         }
     },
@@ -158,9 +153,9 @@ region_resource_id_status = {
     },
     'allowed_ranger_agent_resource_version':
     {
-        'customer': '3.0',
-        'image': '3.0',
-        'flavor': '3.0'
+        'customer': '1.0',
+        'image': '1.0',
+        'flavor': '1.0'
     }
 }
 
