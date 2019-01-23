029d0a1d6a191de52213ab271c4ff0bc8c7913f8
Chi Lo <cl566n@att.com>
2019-01-23 11:50:11 -0800

    Flavor series hiding
    This patch removes all flavor series references in code.
    
    Change-Id: Ief91bb3f05a27262354eef9b2f8db28866360fea

--- orm/services/flavor_manager/config.py
+++ orm/services/flavor_manager/config.py
@@ -31,22 +31,6 @@ database = {
     'connection_string': config.db_connect
 }
 
-# this table is for calculating default extra specs needed
-extra_spec_needed_table = {
-    "p1": {
-        "aggregate_instance_extra_specs____p1": "true",
-        "hw____mem_page_size": "large"
-    }
-}
-
-# any key will be added to extra_spec_needed_table need to be added here
-default_extra_spec_calculated_table = {
-    "aggregate_instance_extra_specs____p1": "",
-    "hw____mem_page_size": "",
-    "hw____cpu_policy": "",
-    "hw____numa_nodes": ""
-}
-
 application_root = 'http://localhost:{0}'.format(server['port'])
 
 api = {
@@ -69,39 +53,6 @@ api = {
         'base': config.audit_server['base_url'],
         'trans': 'v1/audit/transaction'
     }
-
-}
-
-flavor_series = {
-    'valid_series': [
-        'p1'
-    ]
-}
-
-# valid_flavor_options
-flavor_options = {
-    'valid_p1_numa_value': 'n0',
-    'valid_p1_opt_values': [
-        'n0', 'i2'
-    ]
-}
-
-flavor_limits = {
-    # All flavor limits will be converted to integers, and must not be non-numeric.
-    # Root disk, block storage and object storage don't have set limits
-    # vcpu_limit and ephemeral_limit values are in GB
-    # vram_limit and swap_file_limit values are in MB and must be a multiple of 1024
-
-    "swap_file_limit": "327680",
-    "ephemeral_limit": "2000",
-
-    # for 'p1' series:
-    # vcpu_limit and vram_limit for 'p1' series with "n0":"true" option
-    "p1_n0_vcpu_limit": "80",
-    "p1_n0_vram_limit": "327680",
-    # vcpu_limit and vram_limit for 'p1' series with  "n0":"false" option
-    "p1_nx_vcpu_limit": "40",
-    "p1_nx_vram_limit": "163840"
 
 }
 
