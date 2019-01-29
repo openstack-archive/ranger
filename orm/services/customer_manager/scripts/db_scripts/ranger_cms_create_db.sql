5a9f09502ac810e659fe32bd8b0f18a2a34a4b8b
stewie925 <st3wty@att.com>
2019-01-29 12:53:06 -0800

    [Keystone-Group] CRUD APIS
    Change-Id: I359b8380dd9a12cf4d58190ff6c639976d518b5d

--- orm/services/customer_manager/scripts/db_scripts/ranger_cms_create_db.sql
+++ orm/services/customer_manager/scripts/db_scripts/ranger_cms_create_db.sql
@@ -94,3 +94,65 @@ create or replace view rds_resource_status_view AS
     (
         SELECT id, resource_id, region, status,
         err_code, operation from resource_status);
+
+create table if not exists cms_domain
+   (
+         id integer auto_increment not null,
+         name varchar(64) not null,
+         primary key (id),
+         unique name_idx (name));
+
+create table if not exists groups
+   (
+         id integer auto_increment not null,
+         uuid varchar(64) not null,
+         domain_id integer not null,
+         name varchar(64) not null,
+         description varchar(255) not null,
+         primary key (id),
+         foreign key (`domain_id`) references `cms_domain` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+         unique uuid_idx (uuid));
+
+
+create table if not exists group_region
+   (
+         region_id integer not null,
+         group_id integer not null,
+         primary key (region_id, group_id),
+         foreign key (`region_id`) references `cms_region` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+         foreign key (`group_id`) references `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+         index region_id (region_id),
+         index group_id_idx (group_id));
+
+create table if not exists group_role
+   (
+         role_id integer not null,
+         group_id integer not null,
+	 region_id integer not null,
+         primary key (role_id, region_id, group_id),
+	 foreign key (role_id) references cms_role(id),
+         foreign key (`group_id`) references `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+         index region_id (region_id),
+         index group_id_idx (group_id));
+
+create table if not exists group_user
+   (
+         group_id integer not null,
+         user_id integer not null,
+         primary key (group_id, user_id),
+         foreign key (`user_id`) references `cms_user` (`id`) ON DELETE CASCADE,
+         foreign key (`group_id`) references `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+         index user_id (user_id),
+         index group_id (group_id));
+
+create table if not exists group_customer
+   (
+         group_id integer not null,
+         customer_id integer not null,
+         region_id integer not null,
+         primary key (group_id, customer_id, region_id),
+         foreign key (`group_id`) references `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+         foreign key (`customer_id`) references `customer` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+         foreign key (`region_id`) references `cms_region` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+         index customer_id_idx (customer_id),
+         index regio_id_idx (region_id));
