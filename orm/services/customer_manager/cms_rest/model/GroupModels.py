300913fa3edbdc69a09e1238397a509d425ca634
Chi Lo <cl566n@att.com>
2019-03-12 10:38:15 -0700

    Implementation of Keystone Group creation
    Change-Id: Id4828f4611ae5b0e054337149082b2a0ec618d53

--- orm/services/customer_manager/cms_rest/model/GroupModels.py
+++ orm/services/customer_manager/cms_rest/model/GroupModels.py
@@ -1,14 +1,38 @@
 from orm.services.customer_manager.cms_rest.logic.error_base import ErrorStatus
 from orm.services.customer_manager.cms_rest.model.Model import Model
+from orm.common.orm_common.utils.cross_api_utils import (get_regions_of_group,
+                                                         set_utils_conf)
+from pecan import conf
 import wsme
 from wsme import types as wtypes
 
 
 class Region(Model):
-    """network model the group
+    """network model the region
     """
-    def __init__(self, id):
-        self.id = id
+    name = wsme.wsattr(wsme.types.text, mandatory=True)
+    type = wsme.wsattr(wsme.types.text, default="single", mandatory=False)
+    status = wsme.wsattr(wsme.types.text, mandatory=False)
+    error_message = wsme.wsattr(wsme.types.text, mandatory=False)
+
+    def __init__(self, name="", type="single", users=[], status="",
+                 error_message=""):
+        """Create a new region.
+
+        :param name:  region name
+        :param type:  region type
+        :param quotas:  quotas ( array of Quota)
+        :param users:   array of users of specific region
+        :param status: status of creation
+        :param error_message: error message if status is error
+        """
+
+        self.name = name
+        self.type = type
+        self.users = users
+        self.status = status
+        if error_message:
+            self.error_message = error_message
 
 
 class Group(Model):
@@ -17,12 +41,13 @@ class Group(Model):
     description = wsme.wsattr(wsme.types.text, mandatory=True)
     name = wsme.wsattr(wsme.types.text, mandatory=True)
     status = wsme.wsattr(wsme.types.text, mandatory=False)
-    domainId = wsme.wsattr(int, mandatory=True)
+    domain_name = wsme.wsattr(wsme.types.text, mandatory=True)
     uuid = wsme.wsattr(wsme.types.text, mandatory=False)
+    enabled = wsme.wsattr(bool, mandatory=True)
     regions = wsme.wsattr([Region], mandatory=False)
 
-    def __init__(self, description="", name="",
-                 regions=[], status="", domainId=1, uuid=None):
+    def __init__(self, description="", name="", enabled=False,
+                 regions=[], status="", domain_name='default', uuid=None):
         """Create a new Group.
 
         :param description:  Server name
@@ -31,7 +56,8 @@ class Group(Model):
         self.description = description
         self.name = name
         self.status = status
-        self.domainId = domainId
+        self.domain_name = domain_name
+        self.enabled = enabled
         self.regions = regions
         if uuid is not None:
             self.uuid = uuid
@@ -44,18 +70,27 @@ class Group(Model):
         if context == "update":
             for region in self.regions:
                 if region.type == "group":
-                    raise ErrorStatus(400, "region type is invalid for update, \'group\' can be only in create")
+                    raise ErrorStatus(400,
+                                      "region type is invalid for update, "
+                                      " \'group\' can be only in create")
 
     def handle_region_group(self):
         regions_to_add = []
-        for region in self.regions[:]:  # get copy of it to be able to delete from the origin
+        # get copy of it to be able to delete from the origin
+        for region in self.regions[:]:
             if region.type == "group":
                 group_regions = self.get_regions_for_group(region.name)
                 if not group_regions:
-                    raise ErrorStatus(404, 'Group {} Not found'.format(region.name))
+                    raise ErrorStatus(
+                        404, 'Group {} Not found'.format(region.name))
                 self.regions.remove(region)
+        # remove duplicates if exist
+        self.regions.extend(set(regions_to_add))
 
-        self.regions.extend(set(regions_to_add))  # remove duplicates if exist
+    def get_regions_for_group(self, group_name):
+        set_utils_conf(conf)
+        regions = get_regions_of_group(group_name)
+        return regions
 
 
 class GroupResult(Model):
@@ -83,33 +118,44 @@ class GroupResultWrapper(Model):
         self.group = group_result
 
 
-""" GroupSummary is a DataObject and contains all the fields defined in GroupSummary structure. """
+""" GroupSummary is a DataObject and contains all the fields
+    defined in GroupSummary structure.
+"""
 
 
 class GroupSummary(Model):
     name = wsme.wsattr(wsme.types.text)
     id = wsme.wsattr(wsme.types.text)
     description = wsme.wsattr(wsme.types.text)
-    domain_id = wsme.wsattr(int, mandatory=True)
+    domain_name = wsme.wsattr(wsme.types.text)
+    enabled = wsme.wsattr(bool, mandatory=True)
     status = wsme.wsattr(wtypes.text, mandatory=True)
+    regions = wsme.wsattr([str], mandatory=True)
 
     def __init__(self, name='', id='', description='',
-                 status="", domain_id=0):
+                 status="", enabled=True, domain_name='default', regions=[]):
         Model.__init__(self)
 
         self.name = name
         self.id = id
         self.description = description
+        self.enabled = enabled
         self.status = status
-        self.domain_id = domain_id
+        self.domain_name = domain_name
+        self.regions = regions
 
     @staticmethod
     def from_db_model(sql_group):
+        regions = [region.region.name for region in
+                   sql_group.group_regions if
+                   region.region_id != -1]
         group = GroupSummary()
         group.id = sql_group.uuid
         group.name = sql_group.name
         group.description = sql_group.description
-        group.domain_id = sql_group.domain_id
+        group.enabled = bool(sql_group.enabled)
+        group.domain_name = sql_group.domain_name
+        group.regions = regions
 
         return group
 
@@ -120,6 +166,33 @@ class GroupSummaryResponse(Model):
     def __init__(self):
         Model.__init__(self)
         self.groups = []
+
+
+""" Region Result Handler """
+
+
+class RegionResult(Model):
+    id = wsme.wsattr(wsme.types.text, mandatory=True)
+    added = wsme.wsattr(wsme.types.text, mandatory=False)
+    links = wsme.wsattr({str: str}, mandatory=True)
+
+    def __init__(self, id, added=None, links={}):
+        self.id = id
+        self.added = added
+        self.links = links
+
+
+class RegionResultWrapper(Model):
+    transaction_id = wsme.wsattr(wsme.types.text, mandatory=True)
+    regions = wsme.wsattr([RegionResult], mandatory=True)
+
+    def __init__(self, transaction_id, regions):
+        regions_result = [RegionResult(region['id'],
+                                       region['added'],
+                                       region['links']) for region in regions]
+
+        self.transaction_id = transaction_id
+        self.regions = regions_result
 
 
 """ ****************************************************************** """
